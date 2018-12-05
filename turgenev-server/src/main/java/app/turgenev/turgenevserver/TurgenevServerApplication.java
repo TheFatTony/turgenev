896c01836fa079c0e747739ecb5ff554aca69ff1
thefattony <anton.alexeyev@outlook.com>
2018-12-05 12:41:46 +0300

    initial project skeleton

---
+++ turgenev-server/src/main/java/app/turgenev/turgenevserver/TurgenevServerApplication.java
@@ -0,0 +1,12 @@
+package app.turgenev.turgenevserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurgenevServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurgenevServerApplication.class, args);
+	}
+}
